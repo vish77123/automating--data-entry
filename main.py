667a6b24f701f21382e82cbbc1455d4dd756faf5
vishu <vishupatel716@gmail.com>
2021-08-26 23:21:46 +0530

    heroku

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
+import requests
 from selenium import webdriver
 import os
 import time
+from bs4 import BeautifulSoup
 
 
-time.sleep(10)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
@@ -12,9 +12,12 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-driver.get("https://ysense.com/")
+
+responce = requests.get(url="https://ysense.com/")
+data = responce.text
+soup = BeautifulSoup(data, "html.parser")
 time.sleep(30)
-name = driver.find_element_by_xpath('//*[@id="main-header-menu"]/ul/li[1]/a')
+name = soup.find(class_="login")
 print(name.text)
 
 
