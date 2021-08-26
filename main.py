dec92ff9521b2b5025ef9575e1dc89fe2584f3c4
vishu <vishupatel716@gmail.com>
2021-08-26 21:27:29 +0530

    make it better

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument('--disable-gpu')
+
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 driver.get("https://cutt.ly/KWecMLq")
-time.sleep(8)
+time.sleep(3)
 name = driver.find_element_by_id("listing-type")
-print(name)
+print(name.text)
 
 
 
