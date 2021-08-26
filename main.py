1fd8665de629f7b139371a0a4c0c1fc3f2d71931
vishu <vishupatel716@gmail.com>
2021-08-26 21:45:01 +0530

    make it better

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import os
 import time
 
 
+def run():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-driver.get("https://cutt.ly/KWecMLq")
-time.sleep(3)
-name = driver.find_element_by_id("listing-type")
-print(name.text)
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver.get("https://cutt.ly/KWecMLq")
+    time.sleep(3)
+    name = driver.find_element_by_id("listing-type")
+    print(name.text)
 
 
 
-driver.quit()
+    driver.quit()
+
+if __name__ == '__main__':
+    run()
